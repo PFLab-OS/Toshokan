37a1f3de9f9434ce6aadd53333bc9eb782be5bbe
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-11-21 14:42:45 +0900

    Minor changes

--- sync/arm_provision.sh
+++ sync/arm_provision.sh
@@ -2,12 +2,14 @@
 
 test -f /etc/arm_qemu_guest && exit
 
-sudo sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost ubuntu/" /etc/hosts
-
-sudo sed -i'~' -E "s@http://(..\.)?(archive|security)\.ubuntu\.com/ubuntu@http://linux.yz.yamagata-u.ac.jp/pub/linux/ubuntu-archive/@g" /etc/apt/sources.list
+sudo sed -i'~' -E \
+    "s@http://(..\.)?(archive|security)\.ubuntu\.com/ubuntu@http://linux.yz.yamagata-u.ac.jp/pub/linux/ubuntu-archive/@g" \
+    /etc/apt/sources.list
 sudo DEBIAN_FRONTEND=noninteractive apt -qq update
 
 sudo DEBIAN_FRONTEND=noninteractive apt -qq install -y \
     build_essential
+
+sudo sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost ubuntu/" /etc/hosts
 
 sudo sh -c 'date > /etc/arm_qemu_guest'
