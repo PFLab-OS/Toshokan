37ff460decfabe2554c685aef31d1da5cba9f1c8
Shinichi Awamoto <sap.pcmail@gmail.com>
2018-04-27 12:04:23 +0900

    refactoring

--- trampoline/main.cc
+++ trampoline/main.cc
@@ -3,65 +3,112 @@ using int32_t = int;
 
 void panic();
 
-int32_t get_type(char *channel) {
-  return *((int32_t *)channel);
-}
-
-void set_type(char *channel, int32_t type) {
-  *((int32_t *)channel) = type;
-}
-
-void read(char *channel, int offset, uint8_t &data) {
-  data = channel[offset + 4];
-}
-
-void write(char *channel, int offset, uint8_t data) {
-  channel[offset + 4] = data;
-}
-
-void write_string(char *channel, const char *str) {
-  while(true) {
-    while(get_type(channel) != 0) {
-      asm volatile("":::"memory");
-    }
-
-    write(channel, 0, *str);
-  
-    set_type(channel, 2);
-    
-    if (*str == '\0') {
-      break;
-    }
-    
-    str++;
+class Channel {
+public:
+  int32_t GetType() {
+    return *((int32_t *)_address);
   }
+  void SetType(int32_t type) {
+    *((int32_t *)_address) = type;
+  }
+  void Read(int offset, uint8_t &data) {
+    data = _address[offset + 4];
+  }
+  void Write(int offset, uint8_t data) {
+    _address[offset + 4] = data;
+  }
+  void WriteString(const char *str) {
+    while(true) {
+      while(GetType() != 0) {
+	asm volatile("pause":::"memory");
+      }
+
+      Write(0, *str);
+  
+      SetType(2);
+    
+      if (*str == '\0') {
+	break;
+      }
+    
+      str++;
+    }
+  }
+  void WaitNewSignal() {
+    while(GetType() == 0) {
+      asm volatile("pause":::"memory");
+    }
+  }
+  int SendSignal(int32_t type) {
+    int rval;
+    if (type == 0) {
+      return 0;
+    }
+    SetType(type);
+
+    while((rval = GetType()) != type) {
+      asm volatile("pause":::"memory");
+    }
+    return rval;
+  }
+protected:
+  Channel() {
+  }
+  char *_address;
+};
+
+class H2F : public Channel {
+public:
+  H2F() {
+    _address = (char *)0x2000;
+  }
+};
+
+class F2H : public Channel {
+public:
+  F2H() {
+    _address = (char *)0x3000;
+  }
+};
+
+// callback test
+void callback(H2F &h2f, F2H &f2h) {
+  h2f.SetType(0);
+  f2h.SetType(1);
 }
 
-bool do_signal() {
-  char *h2f = (char *)0x2000;
-  char *f2h = (char *)0x3000;
-  switch(get_type(h2f)) {
-  case 0:
-    // NOP
-    return true;
-  case 1:
-    // callback test
-    set_type(h2f, 0);
-    set_type(f2h, 1);
-    return false;
-  case 2:
-    // print request test
-    set_type(h2f, 0);
+// print request test
+void print(H2F &h2f, F2H &f2h) {
+  h2f.SetType(0);
+  f2h.WriteString("abc\n");
+}
 
-    write_string(f2h, "abc\n");
-    return false;
+// execute binary
+void exec_bin(H2F &h2f, F2H &f2h) {
+  h2f.SetType(-1);
+}
+
+void do_signal(H2F &h2f) {
+  F2H f2h;
+  switch(h2f.GetType()) {
+  case 0:
+    panic();
+  case 1:
+    callback(h2f, f2h);
+    break;
+  case 2:
+    print(h2f, f2h);
+    break;
+  case 3: 
+    exec_bin(h2f, f2h);
+    break;
   }
 }
 
 extern "C" void trampoline_main() {
-  while(do_signal()) {
-    asm volatile("":::"memory");
-  }
+  H2F h2f;
+  h2f.WaitNewSignal();
+  do_signal(h2f);
   panic();
 }
 
