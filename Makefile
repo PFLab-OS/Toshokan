c6920a9fa08be448431f3bc66975bc1d9aacaf02
Shinichi Awamoto <sap.pcmail@gmail.com>
2018-05-03 15:21:48 +0900

    support new FriendLoader interface

--- Makefile
+++ Makefile
@@ -1,36 +1,25 @@
 CXXFLAGS = -g -O0 -Wall --std=c++14
 
 init: init.cc
-	g++ $(CXXFLAGS) init.cc
+	g++ $(CXXFLAGS) $^ -o $@
 
 callback: callback.cc
-	g++ $(CXXFLAGS) callback.cc
+	g++ $(CXXFLAGS) $^ -o $@
 
 print: print.cc
-	g++ $(CXXFLAGS) print.cc
+	g++ $(CXXFLAGS) $^ -o $@
 
 exec_bin: exec_bin.cc
-	g++ $(CXXFLAGS) exec_bin.cc
-
-stop:
-	-sudo rmmod friend_loader.ko
-
-reset:
-	make stop
-	cd ../FriendLoader; ./run.sh
+	g++ $(CXXFLAGS) $^ -o $@
 
 test:
-	make reset
-	make init
-	sudo ./a.out
-	make reset
-	make callback
-	sudo ./a.out
-	make reset
-	make print
-	sudo ./a.out
-	make reset
-	make exec_bin
-	sudo ./a.out
-	make stop
+	cd ../FriendLoader; ./run.sh load; ./run.sh run
+	make init; sudo ./init
+	cd ../FriendLoader; ./run.sh restart
+	make callback; sudo ./callback
+	cd ../FriendLoader; ./run.sh restart
+	make print; sudo ./print
+	cd ../FriendLoader; ./run.sh restart
+	#make exec_bin; sudo ./exec_bin
+	cd ../FriendLoader; ./run.sh unload
 	@echo "All tests have successfully finished!"
---
+++ exec_bin.cc
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include "channel.h"
+
+int main(int argc, char **argv) {
+  int configfd_h2f = open("/sys/module/friend_loader/call/h2f", O_RDWR);
+  int configfd_f2h = open("/sys/module/friend_loader/call/f2h", O_RDWR);
+  if(configfd_h2f < 0 || configfd_f2h < 0) {
+    perror("Open call failed");
+    return -1;
+  }
+
+  char *h2f_address = static_cast<char *>(mmap(nullptr, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, configfd_h2f, 0));
+  char *f2h_address = static_cast<char *>(mmap(nullptr, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, configfd_f2h, 0));
+  if (h2f_address == MAP_FAILED || f2h_address == MAP_FAILED) {
+    perror("mmap operation failed");
+    return -1;
+  }
+  F2H f2h(f2h_address);
+  H2F h2f(h2f_address);
+
+  if (h2f.SendSignal(3) != 0) {
+    printf("test: failed\n");
+    return -1;
+  }
+
+  printf("test: OK\n");
+    
+  close(configfd_h2f);
+  close(configfd_f2h);
+  return 0;
+}
