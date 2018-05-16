0429729c431fb8d59a877c48ed506905514bcd14
Shinichi Awamoto <sap.pcmail@gmail.com>
2018-05-16 23:00:04 +0900

    tiny fix

--- result.h
+++ result.h
@@ -11,14 +11,14 @@ public:
   }
   ~Result() {
     if (!_checked) {
-      fprintf(stderr, "check the result\n");
+      fprintf(stderr, "Result: error: check the result\n");
       exit(255);
     }
   }
   T Unwrap() {
     _checked = true;
     if (_error) {
-      fprintf(stderr, "failed to unwrap\n");
+      fprintf(stderr, "Result: error: failed to unwrap\n");
       exit(255);
     }
     return _t;
@@ -28,7 +28,7 @@ public:
   }
   void IgnoreError() {
     if (!_error) {
-      fprintf(stderr, "do not call IgnoreError() when there is no error.\n");
+      fprintf(stderr, "Result: error: do not call IgnoreError() when there is no error.\n");
       exit(255);
     }
     _checked = true;
