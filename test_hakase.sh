c19fbd2f273bb6ae2a1960201151999269110c19
liva <sap.pcmail@gmail.com>
2018-06-01 11:33:05 +0900

    cleanup hakase when 'make test' is terminated by SIGINT

--- test_hakase.sh
+++ test_hakase.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
+# arg1: If crash test or not.
+# arg2: binary file name
+# arg3...: arguments
 
 cd `dirname $0`
 if [ -f $2 ]; then
@@ -6,13 +9,18 @@ if [ -f $2 ]; then
     ./run.sh run
     popd
 
+    trap 'pushd ../FriendLoader; ./run.sh stop; popd' SIGINT
+
     sudo ./test_library.sh $*
     rval=$?
+
+    trap SIGINT
     
     pushd ../FriendLoader
     ./run.sh stop
     popd
     exit $rval
 else
+    # will be fail in test_library.sh
     ./test_library.sh $*
 fi
