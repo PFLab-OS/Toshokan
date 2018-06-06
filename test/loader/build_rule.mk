c9ae8c0f2b860f26a9b9508495632f8bce9369de
liva <sap.pcmail@gmail.com>
2018-06-06 13:51:58 +0900

    enable loader to load from files

--- test/loader/build_rule.mk
+++ test/loader/build_rule.mk
@@ -6,14 +6,11 @@ default: test
 raw.bin: raw.cc
 	g++ -O0 -Wall --std=c++14 -fpie -nostdinc -nostdlib -iquote $(INCLUDE_DIR) -T raw.ld $^ -o $@
 
-raw_bin.o: raw.bin
-	objcopy -I binary -O elf64-x86-64 -B i386:x86-64 $^ $@
-
-exec.bin: exec.cc raw_bin.o ../test.cc
+exec.bin: exec.cc ../test.cc
 	g++ $(CXXFLAGS) $^ -o $@
 
 test:
-	@$(foreach test, $(TESTS), $(MAKE) $(test).bin; ../test_hakase.sh 0 $(shell pwd)/$(test).bin; )
+	@$(foreach test, $(TESTS), $(MAKE) $(test).bin; ../test_hakase.sh 0 $(shell pwd)/$(test).bin $(shell pwd)/raw.bin; )
 
 clean:
 	rm -f *.bin raw_bin.o
--- test/loader/exec.cc
+++ test/loader/exec.cc
@@ -1,27 +1,57 @@
 #include <assert.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <vector>
 #include "../test.h"
 #include "channel.h"
 #include "memrw.h"
 
 static const uint64_t kDeployAddressStart = 0x100000;
 
-void deploy(H2F &h2f) {
-  extern uint8_t _binary_raw_bin_start[];
-  extern uint8_t _binary_raw_bin_end[];
-  extern uint8_t _binary_raw_bin_size[];
-  size_t binary_raw_bin_size = reinterpret_cast<size_t>(_binary_raw_bin_size);
-  assert(_binary_raw_bin_start + binary_raw_bin_size == _binary_raw_bin_end);
+int deploy(H2F &h2f, const char *fname) {
+  FILE *fp;
+
+  fp = fopen(fname, "rb");
+  if (fp == NULL){
+    return 1;
+  }
+
+  std::vector<std::pair<size_t, uint8_t *>> data;
+
+  while(true) {
+    uint8_t *buf = new uint8_t[2048];
+    size_t sz = fread(buf, 1, 2048, fp);
+    if (sz == 0) {
+      delete buf;
+      break;
+    }
+    assert(sz < 2048);
+    std::pair<size_t, uint8_t *> p = std::make_pair(sz, buf);
+    data.push_back(p);
+  }
+
+  fclose(fp);
 
   uint64_t deploy_address = kDeployAddressStart;
-  for(uint8_t *addr = _binary_raw_bin_start; addr < _binary_raw_bin_end; addr += 2048, deploy_address += 2048) {
-    MemoryAccessor::Writer mw(h2f, deploy_address, MemoryAccessor::DataSize::Create(_binary_raw_bin_end - addr > 2048 ? 2048 : _binary_raw_bin_end - addr).Unwrap());
-    mw.Copy(addr).Unwrap();
+  for (auto it = data.begin(); it != data.end(); it++) {
+    MemoryAccessor::Writer mw(h2f, deploy_address, MemoryAccessor::DataSize::Create((*it).first).Unwrap());
+    mw.Copy((*it).second).Unwrap();
     mw.Do().Unwrap();
+    delete (*it).second;
+    deploy_address += (*it).first;
   }
+
+  return 0;
 }
 
 int test_main(F2H &f2h, H2F &h2f, int argc, const char **argv) {
-  deploy(h2f);
+  if (argc < 2) {
+    return 1;
+  }
+  
+  if (deploy(h2f, argv[1]) != 0) {
+    return 1;
+  }
 
   h2f.Reserve();
   h2f.Write(0, kDeployAddressStart);
