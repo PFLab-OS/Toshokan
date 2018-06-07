6cb6ac12c6bd76606ff09b3b19bedcb9a07d9065
liva <sap.pcmail@gmail.com>
2018-06-07 09:28:30 +0900

    bug (test continues if building files is failed) fix

--- test/memrw/build_rule.mk
+++ test/memrw/build_rule.mk
@@ -7,7 +7,7 @@ default: test
 	g++ $(CXXFLAGS) $^ -o $@
 
 test:
-	@$(foreach test, $(TESTS), $(MAKE) $(test).bin; ../test_hakase.sh 0 $(shell pwd)/$(test).bin; )
+	@$(foreach test, $(TESTS), $(MAKE) $(test).bin && ../test_hakase.sh 0 $(shell pwd)/$(test).bin &&) :
 
 clean:
 	rm -f *.bin
--- test/simple_loader/build_rule.mk
+++ test/simple_loader/build_rule.mk
@@ -10,7 +10,7 @@ hakase.bin: hakase.cc ../test.cc ../../simple_loader/hakase.cc
 	g++ $(CXXFLAGS) $^ -o $@
 
 test:
-	@$(foreach test, $(TESTS), $(MAKE) $(test).bin; ../test_hakase.sh 0 $(shell pwd)/$(test).bin $(shell pwd)/raw.bin; )
+	@$(foreach test, $(TESTS), $(MAKE) $(test).bin && ../test_hakase.sh 0 $(shell pwd)/$(test).bin $(shell pwd)/raw.bin &&) :
 
 clean:
 	rm -f *.bin raw_bin.o
