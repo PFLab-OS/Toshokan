9fcda77c5f5312c2777fe8c0f6d48798cb440fde
liva <sap.pcmail@gmail.com>
2018-06-10 17:13:28 +0900

    improve the test of elf_loader

--- elf_loader/test/friend.cc
+++ elf_loader/test/friend.cc
@@ -1,11 +1,18 @@
 using uint64_t = __UINT64_TYPE__;
 #include "common/_memory.h"
 
-int buf[10]; // for .bss
+int buf1[10]; // for .bss
+int buf2[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // for .data
 
 int main();
 
 int main() {
+  for (int i = 0; i < 10; i++) {
+    if (buf1[i] != 0 || buf2[i] != i) {
+      asm volatile("cli;hlt;hlt;");
+    }
+  }
+  
   int *channel = reinterpret_cast<int *>(MemoryMap::kF2h);
   channel[0] = 1;
   asm volatile("cli;hlt;hlt;");
