2fb454b99b706f6e885469f66ccc9a3a3c96ee71
mumumu <mumumu0722@gmail.com>
2018-06-25 15:55:17 +0900

    Added friendOS loading feature after joshu's setup

--- joshu/test/friend.cc
+++ joshu/test/friend.cc
@@ -2,6 +2,8 @@ using uint64_t = __UINT64_TYPE__;
 #include "common/_memory.h"
 
 int main() {
-  while(1) {}
+  int *channel = reinterpret_cast<int *>(MemoryMap::kF2h);
+  channel[0] = 7;
+
   return 0;
 }
