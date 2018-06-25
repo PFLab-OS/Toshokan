ce7340172925681dbe907557bbd059632067b957
mumumu <mumumu0722@gmail.com>
2018-06-25 15:55:17 +0900

    Added friendOS loading feature after joshu's setup

--- joshu/test/hakase.cc
+++ joshu/test/hakase.cc
@@ -5,7 +5,7 @@
 int test_main(F2H &f2h, H2F &h2f, int argc, const char **argv) {
   auto ic = InterruptController(f2h);
 
-  if (argc < 2) {
+  if (argc < 3) {
     return 1;
   }
 
@@ -47,6 +47,46 @@ int test_main(F2H &f2h, H2F &h2f, int argc, const char **argv) {
   if(f2h.WaitNewSignal() != 6) { 
     return 1;
   }
+
+  // Load friend.bin
+  auto file2 = ElfLoader::ElfFile::Load(argv[2]);
+  if (!file2) {
+    return 1;
+  }
+
+  {
+    auto r = file2->Init(h2f);
+    if (r.IsError()) {
+      r.IgnoreError();
+      return 1;
+    }
+    r.Unwrap();
+  }
+  
+  ElfLoader sl2(h2f, std::move(file2));
+
+  {
+    auto r = sl2.Deploy();
+    if (r.IsError()) {
+      r.IgnoreError();
+      return 1;
+    }
+    r.Unwrap();
+  }
+
+  {
+    auto r = sl2.Execute();
+    if (r.IsError()) {
+      r.IgnoreError();
+      return 1;
+    }
+    r.Unwrap();
+  }
+
+  if(f2h.WaitNewSignal() != 7) { 
+    return 1;
+  }
+
 
   return 0;
 }
