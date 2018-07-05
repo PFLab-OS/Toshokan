c6ecd3b6bb29cca25de0f7324d0d925b3bd93fc0
Liva <sap.pcmail@gmail.com>
2018-07-05 08:55:41 +0000

    remove debug codes

--- hakase/callback/test/hakase.cc
+++ hakase/callback/test/hakase.cc
@@ -3,10 +3,8 @@
 
 int test_main(F2H &f2h, H2F &h2f, int argc, const char **argv) {
   int16_t id = 1;
-  printf("!!!!\n");
   Channel::Accessor ch_ac(h2f, id);
   ch_ac.Do(1);
-  printf("!!!!\n");
 
   int16_t type;
   if (f2h.WaitNewSignal(type) != id) {
