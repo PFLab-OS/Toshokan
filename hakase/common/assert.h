293867da0a2bbb7e392886836cc6e9642789a71e
liva <sap.pcmail@gmail.com>
2018-07-15 03:49:11 +0900

    tiny fix

--- hakase/common/assert.h
+++ hakase/common/assert.h
@@ -7,6 +7,6 @@
 #elif defined(__FRIEND__)
 
 #include "common/panic.h"
-#define assert(flag) if (!flag) { panic(); }
+#define assert(flag) if (!(flag)) { panic(); }
 
 #endif /* defined(__HAKASE__) */
