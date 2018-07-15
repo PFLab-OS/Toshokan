3b970d5b7e27077568d6e1aa1df8e5bdadfa012c
liva <sap.pcmail@gmail.com>
2018-07-15 14:44:09 +0900

    add sample/paging/100

---
+++ sample/paging/100/friend.cc
@@ -0,0 +1,61 @@
+#include "common/type.h"
+#include "common/_memory.h"
+#include "common/channel.h"
+
+int16_t get_cpuid() {
+  int16_t id;
+  asm volatile("movw %%fs:0x4, %0" : "=r"(id));
+  return id;
+}
+
+void puts(F2H &f2h, const char *str) {
+  while(*str) {
+    Channel::Accessor<1> ch_ac(f2h, 2);
+    ch_ac.Write<char>(0, *str);
+    ch_ac.Do(0);
+    str++;
+  }
+  Channel::Accessor<1> ch_ac(f2h, 2);
+  ch_ac.Write<char>(0, '\n');
+  ch_ac.Do(0);
+}
+
+void return_value(F2H &f2h, int i) {
+  Channel::Accessor<sizeof(int)> ch_ac(f2h, 1);
+  ch_ac.Write<int>(0, i);
+  ch_ac.Do(0);
+}
+
+int main() {
+  F2H f2h;
+
+  puts(f2h, "booted");
+
+  uint64_t *pml4t = (uint64_t *)0x301000UL;
+  uint64_t *pdpt  = (uint64_t *)0x302000UL;
+  uint64_t *pd    = (uint64_t *)0x303000UL;
+
+  for (int i = 0; i < 512; i++) {
+    pml4t[i] = 0;
+    pdpt[i] = 0;
+    pd[i] = 0;
+  }
+  
+  pml4t[0] = ((uint64_t)pdpt + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
+  pdpt[0]  = ((uint64_t)pd + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2) | (1 << 7);
+  pd[0]    = 0x80000000UL | (1 << 0) | (1 << 1) | (1 << 2) | (1 << 7);
+  pd[1]    = 0x80200000UL | (1 << 0) | (1 << 1) | (1 << 2) | (1 << 7);
+  //!!!!!!!!!!!!!!!!!!!!!!
+  // add your codes here!
+  //!!!!!!!!!!!!!!!!!!!!!!
+  
+  asm volatile("movq %0, %%cr3"::"r"((uint64_t)pml4t + 0x80000000UL));
+  
+  if (*((uint64_t *)0x200000UL) == *((uint64_t *)0x40000000UL)) {
+    return_value(f2h, 0);
+  } else {
+    return_value(f2h, 1);
+  }
+  
+  return 0;
+}
