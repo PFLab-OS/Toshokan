f118b5a4458e46a6937ad7b3ac5a4e225c5f3286
liva <sap.pcmail@gmail.com>
2018-07-16 15:03:33 +0900

    tiny fix

--- hakase/test_build_rule.mk
+++ hakase/test_build_rule.mk
@@ -10,20 +10,26 @@ DEPLOY_FILES:=$(TEST_BINS) $(EX_DEPLOY_FILES)
 
 -include $(DEPS)
 
-.PHONY: test clean pre-deploy
+.PHONY: FORCE test bin clean pre-deploy
+.PRECIOUS: %.o
+
+FORCE:
 
 %.bin: %.o $(TEST_DIR)test.o $(foreach lib, $(LIBRARIES), $(MODULE_DIR)lib$(lib).a)
 	g++ $(CXXFLAGS) -o $@ $^
 
-test:
+test: FORCE
 	$(MAKE) -C $(ROOT_DIR) prepare_test
 	$(MAKE) pre-deploy
 	$(MAKE) -C $(ROOT_DIR) execute_test
 
-clean:
+bin: $(TEST_BINS) $(DEPLOY_FILES)
+
+clean: FORCE
 	rm -f *.bin $(OBJS) $(DEPS) $(TMP_FILES)
 
-pre-deploy: $(TEST_BINS) $(DEPLOY_FILES)
+pre-deploy: FORCE
+	$(MAKE) bin
 	mkdir -p $(DEPLOY_DIR)$(RELATIVE_DIR)/
 	$(foreach file, $(DEPLOY_FILES), cp $(file) $(DEPLOY_DIR)$(RELATIVE_DIR)/$(file) &&) :
 	$(foreach file, $(TEST_BINS), echo "$(QEMU_DIR)/test_hakase.sh 0 $(QEMU_DIR)$(RELATIVE_DIR)/$(file) $(ARGUMENTS)" >> $(RUN_SCRIPT) &&) :
