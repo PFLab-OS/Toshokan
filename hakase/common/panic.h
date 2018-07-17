5f4ec891a835b2f25c7413937a9b3883391a7b65
Shinichi Awamoto <sap.pcmail@gmail.com>
2018-07-18 01:28:17 +0900

    bug fix

--- hakase/common/panic.h
+++ hakase/common/panic.h
@@ -2,11 +2,11 @@
 
 #if defined(__HAKASE__)
 
-#define panic() assert(false)
+#define panic(x) assert(false)
 
 #elif defined(__FRIEND__)
 
-static inline void panic() {
+static inline void panic(const char *str=nullptr) {
   while(true) {
     __asm__ volatile("cli;hlt;");
   }
--- hakase/common/result.h
+++ hakase/common/result.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <stdio.h>
+#include "panic.h"
 
 template<class T>
 class Result {
