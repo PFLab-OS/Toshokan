8d7703dfe6b95b1b295804b6218a7a4aa3260d2f
liva <sap.pcmail@gmail.com>
2018-07-20 17:55:11 +0900

    add friend dir to format paths

--- friend/panic.h
+++ friend/panic.h
@@ -1,7 +1,7 @@
 #pragma once
 
-static inline void panic(const char *str=nullptr) {
-  while(true) {
+static inline void panic(const char *str = nullptr) {
+  while (true) {
     __asm__ volatile("cli;hlt;");
   }
 }
--- friend/type.h
+++ friend/type.h
@@ -2,4 +2,3 @@
 
 #include <stdint.h>
 using size_t = uint64_t;
-
