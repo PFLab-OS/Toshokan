135e57b0e95ae557deb8ec081f5935249ae6875a
liva <sap.pcmail@gmail.com>
2018-07-21 00:00:36 +0900

    changed to use clang-format container

--- hakase/build_rule.mk
+++ hakase/build_rule.mk
@@ -1,4 +1,3 @@
-DOCKER_IMAGE_TAG=954adad3c90db9288aaaad82d53ffe24f0df869f
 ROOT_DIR:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 RELATIVE_DIR:=$(shell bash -c "root_dir=$(abspath $(ROOT_DIR)../); pwd=$(CURDIR); echo \$${pwd\#\$${root_dir}};")
 DEPLOY_DIR:=/deploy
@@ -10,7 +9,10 @@ ifneq ($(HOST),)
 # host environment
 HOST_DIR:=$(abspath $(ROOT_DIR)../)
 SHARE_DIR:=/share
-CONTAINER_NAME:=toshokan
+BUILD_CONTAINER:=livadk/hakase-qemu:63e6659f7929854cb72aac6223b7ce424de34a6f
+BUILD_CONTAINER_NAME:=toshokan
+FORMAT_CONTAINER:=livadk/clang-format:9f1d281b0a30b98fbb106840d9504e2307d3ad8f
+FORMAT_CONTAINER_NAME:=toshokan_format
 
 ifeq ($(OS),Windows_NT)
 CRLF_CHECK:=! find $(HOST_DIR)/hakase -not -type d -exec file "{}" ";" | grep CRLF || sh -c 'echo "****CRLF detected from the files!****\nplease follow these steps!(Warning: It will discard your changes)\n$$ git config core.autocrlf false\n$$ git reset --hard HEAD"; exit 1'
@@ -21,28 +23,48 @@ define make_wrapper
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo  Running \"make$1\" on the docker environment.
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@docker rm $(CONTAINER_NAME) -f > /dev/null 2>&1 || :
-	docker run -d $(if $(CI),,-v $(HOST_DIR):$(SHARE_DIR)) -it --name $(CONTAINER_NAME) livadk/hakase-qemu:$(DOCKER_IMAGE_TAG)
-	$(if $(CI),docker cp $(HOST_DIR) $(CONTAINER_NAME):$(SHARE_DIR))
+	@docker rm $1 -f > /dev/null 2>&1 || :
+	docker run -d $(if $(CI),,-v $(HOST_DIR):$(SHARE_DIR)) -it --name $1 $2
+	$(if $(CI),docker cp $(HOST_DIR) $1:$(SHARE_DIR))
 	@echo ""
-	@echo 'docker exec $(CONTAINER_NAME) sh -c "cd /share$(RELATIVE_DIR) && make$1"'
-	@bash -c "trap \"docker exec $(CONTAINER_NAME) sh -c 'pkill qemu-system-x86 || :'\" INT ERR; docker exec $(CONTAINER_NAME) sh -c \"cd /share$(RELATIVE_DIR) && make$1\""
+	@echo 'docker exec $1 sh -c "cd /share$(RELATIVE_DIR) && make$3"'
+	@bash -c "trap \"docker exec $1 sh -c 'pkill qemu-system-x86 || :'\" INT ERR; docker exec $1 sh -c \"cd /share$(RELATIVE_DIR) && make$4\""
 	@echo ""
-	docker rm -f $(CONTAINER_NAME)
+	docker rm -f $1
+endef
+
+define docker_wrapper
+	@docker rm $1 -f > /dev/null 2>&1 || :
+	docker run -d $(if $(CI),,-v $(HOST_DIR):$(SHARE_DIR)) -it --name $1 $2
+	$(if $(CI),docker cp $(HOST_DIR) $1:$(SHARE_DIR))
+	docker exec $1 sh -c "cd /share$(RELATIVE_DIR) && $3"
+	@echo ""
+	docker rm -f $1
 endef
 
 .DEFAULT_GOAL:=default
 default:
-	$(call make_wrapper,)
+	$(call make_wrapper,$(BUILD_CONTAINER_NAME),$(BUILD_CONTAINER))
 
 attach_docker:
-	docker exec -it $(CONTAINER_NAME) /bin/bash
+	docker exec -it $(BUILD_CONTAINER_NAME) /bin/bash
 
 run_docker:
-	docker run --rm -v $(HOST_DIR):$(SHARE_DIR) -it livadk/hakase-qemu:$(DOCKER_IMAGE_TAG) /bin/bash
+	docker run --rm -v $(HOST_DIR):$(SHARE_DIR) -it $(BUILD_CONTAINER) /bin/bash
+
+format:
+	@echo "Formatting with clang-format. Please wait..."
+	@$(call docker_wrapper,$(FORMAT_CONTAINER_NAME),$(FORMAT_CONTAINER),\
+	 git ls-files .. \
+	  | grep -e FriendLoader/ \
+	         -e ../friend/ \
+	  | grep -E '.*\.cc$$|.*\.h$$' \
+		| xargs -n 1 clang-format -style="{BasedOnStyle: Google}" -i \
+	 $(if $(CI),&& git diff --quiet))
+	@echo "Done."
 
 %:
-	$(call make_wrapper, $@)
+	$(call make_wrapper,$(BUILD_CONTAINER_NAME),$(BUILD_CONTAINER), $@)
 else
 # guest environment
 ifneq ($(PWD),$(CURDIR))
