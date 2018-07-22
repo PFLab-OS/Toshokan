499594d75f86b3a2b87c8f78fdd3c79dd23db6a6
liva <sap.pcmail@gmail.com>
2018-07-22 15:07:59 +0900

    modified paging/001

--- sample/paging/001/friend.cc
+++ sample/paging/001/friend.cc
@@ -27,21 +27,34 @@ void return_value(F2H &f2h, int i) {
   ch_ac.Do(0);
 }
 
+void memset(void *addr, int ch, size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    ((uint8_t *)addr)[i] = ch;
+  }
+}
+
 int main() {
   F2H f2h;
 
   puts(f2h, "hello!");
 
+  memset((void *)0x310000UL, 0, 0x5000);
+
   uint64_t *pml4t = (uint64_t *)0x310000UL;
   uint64_t *pdpt  = (uint64_t *)0x311000UL;
   uint64_t *pd1   = (uint64_t *)0x312000UL;
   uint64_t *pd2   = (uint64_t *)0x313000UL;
+  uint64_t *pt1   = (uint64_t *)0x314000UL;
 
   pml4t[0] = ((uint64_t)pdpt + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
   pdpt[0]  = ((uint64_t)pd1 + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
   pdpt[3]  = ((uint64_t)pd2 + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
+  pd2[0]   = ((uint64_t)pt1 + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
   for(int i = 0; i < 512; i++) {
-    pd1[i]  = (0x80000000UL + 0x200000UL * i) | (1 << 0) | (1 << 1) | (1 << 2) | (1<<7);
+    pd1[i]  = (0x80000000UL + 0x200000UL * i) | (1 << 0) | (1 << 1) | (1 << 2) | (1 << 7);
+  }
+  for(int i = 0; i < 512; i++) {
+    pt1[i]  = (0x80000000UL + 0x1000UL * i) | (1 << 0) | (1 << 1) | (1 << 2);
   }
   asm volatile("movq %0, %%cr3"::"r"((uint64_t)pml4t + 0x80000000UL));
 
