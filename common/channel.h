fbc5d574990c08031f13a3c69f2552e3f3215eac
liva <sap.pcmail@gmail.com>
2018-07-28 22:23:59 +0900

    implement new channel(Channel2)

---
+++ common/channel.h
@@ -0,0 +1,104 @@
+#pragma once
+#include <assert.h>
+#include "type.h"
+#include "result.h"
+
+/*
+ * Channel : communication channel between hakase and friend
+ * !!! Do not call Channel class directly. Use ChannelAccessor instead. !!!
+ *
+ * Each Id must be unique in a system.
+ */
+class Channel2 {
+public:
+  class Id {
+  public:
+    Id() = delete;
+    Id(int32_t val) : _val(val) {
+    }
+    Id(const Id &id) : _val(id._val) {
+    }
+    bool operator==(const Id &id) {
+      return id._val == _val;
+    }
+    bool operator!=(const Id &id) {
+      return id._val != _val;
+    }
+    Id &operator=(const Id &id) {
+      _val = id._val;
+      return *this;
+    }
+    static bool CompareAndSwap(Id *ptr, const Id &oldval, const Id &newval) {
+      return __sync_bool_compare_and_swap(&ptr->_val, oldval._val, newval._val);
+    }
+    static Id Null() {
+      return Id(-1);
+    }
+  private:
+    int32_t _val;
+  } __attribute__((packed));
+  
+  Channel2() = delete;
+  Channel2(uint8_t *address, Id my_id) : _address(address), _header(reinterpret_cast<Header *>(address)), _my_id(my_id) {
+  }
+  Result<bool> Reserve() {
+    const Id null = Id::Null();
+    if (Id::CompareAndSwap(&_header->src_id, null, _my_id)) {
+      _header->type = 0;
+      return Result<bool>(true);
+    } else {
+      return Result<bool>();
+    }
+  }
+  void SendSignal(Id dest, int32_t type) {
+    assert(type != 0);
+    assert(_header->src_id == _my_id);
+    assert(_header->dest_id == Id::Null());
+    assert(_header->type == 0);
+    _header->dest_id = dest;
+    _header->type = type;
+  }
+  Result<int32_t> CheckIfReturned() {
+    asm volatile("":::"memory");
+    assert(_header->src_id == _my_id);
+    if (_header->type == 0) {
+      return Result<int32_t>(_header->rval);
+    } else {
+      return Result<int32_t>();
+    }
+  }
+  void Release() {
+    assert(_header->src_id == _my_id);
+    assert(_header->type == 0);
+    _header->dest_id = Id::Null();
+    _header->src_id = Id::Null();
+  }
+  Result<int32_t> CheckIfNewSignalArrived() {
+    asm volatile("":::"memory");
+    if (_header->dest_id == _my_id && _header->type != 0) {
+      return Result<int32_t>(_header->type);
+    } else {
+      return Result<int32_t>();
+    }
+  }
+  void Return(int32_t rval) {
+    _header->rval = rval;
+    _header->type = 0;
+  }
+  static void InitBuffer(uint8_t *address) {
+    Header *header = reinterpret_cast<Header *>(address);
+    header->src_id = Id::Null();
+    header->dest_id = Id::Null();
+  }
+  static const int kBufAddress = 4096;
+private:
+  struct Header {
+    int32_t type;
+    Id src_id;
+    Id dest_id;
+    int32_t rval;
+  } __attribute__((packed));
+  uint8_t * const _address;
+  Header * const _header;
+  const Id _my_id;
+};
---
+++ common/tests/channel.cc
@@ -0,0 +1,204 @@
+#include <future>
+#include <string.h>
+#include <chrono>
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+#include "../channel.h"
+
+TEST_GROUP(Id) {
+  TEST_SETUP() {
+  }
+  TEST_TEARDOWN() {
+  }
+};
+
+TEST(Id, CheckEquality) {
+  Channel2::Id id1(1);
+  Channel2::Id id2(1);
+  CHECK(id1 == id2);
+}
+
+TEST(Id, CheckSubstitution) {
+  Channel2::Id id1(1);
+  Channel2::Id id2 = id1;
+  CHECK(id1 == id2);
+}
+
+TEST(Id, CheckCasSuccess) {
+  Channel2::Id id1(1);
+  Channel2::Id id2 = id1;
+  Channel2::Id id3(2);
+  CHECK(Channel2::Id::CompareAndSwap(&id1, id2, id3));
+  CHECK(id1 == id3);
+}
+
+TEST(Id, CheckCasFail) {
+  Channel2::Id id1(1);
+  Channel2::Id id2 = id1;
+  Channel2::Id id3(2);
+  CHECK_FALSE(Channel2::Id::CompareAndSwap(&id1, id3, id3));
+  CHECK(id1 == id2);
+}
+
+TEST_GROUP(Channel2) {
+  TEST_SETUP() {
+    channel_buf = new uint8_t[Channel2::kBufAddress];
+    memset(channel_buf, 0, Channel2::kBufAddress);
+    Channel2::InitBuffer(channel_buf);
+  }
+
+  TEST_TEARDOWN() {
+    delete [] channel_buf;
+    mock().clear();
+  }
+  uint8_t *channel_buf;
+};
+
+TEST(Channel2, FirstOneCanReserveChannel) {
+  Channel2 ch(channel_buf, Channel2::Id(1));
+  
+  CHECK_FALSE(ch.Reserve().IsError());
+}
+
+TEST(Channel2, NoOneSent) {
+  Channel2 ch(channel_buf, Channel2::Id(1));
+  
+  CHECK(ch.CheckIfNewSignalArrived().IsError());
+}
+
+TEST(Channel2, ReservedButNotSent) {
+  Channel2 ch1(channel_buf, Channel2::Id(1));
+  Channel2 ch2(channel_buf, Channel2::Id(0));
+
+  ch1.Reserve();
+  CHECK(ch2.CheckIfNewSignalArrived().IsError());
+}
+
+TEST(Channel2, NoOneReturned) {
+  Channel2 ch1(channel_buf, Channel2::Id(1));
+  Channel2 ch2(channel_buf, Channel2::Id(0));
+
+  ch1.Reserve();
+  ch1.SendSignal(Channel2::Id(0), 1);
+  ch2.CheckIfNewSignalArrived();
+  CHECK(ch1.CheckIfReturned().IsError());
+}
+
+TEST(Channel2, SendReceiveSignal1) {
+  Channel2 ch1(channel_buf, Channel2::Id(1));
+  Channel2 ch2(channel_buf, Channel2::Id(0));
+  int32_t signal = rand();
+  
+  ch1.Reserve();
+  ch1.SendSignal(Channel2::Id(0), signal);
+  CHECK_EQUAL(signal, ch2.CheckIfNewSignalArrived().Unwrap());
+}
+
+TEST(Channel2, SendReceiveSignal2) {
+  Channel2 ch1(channel_buf, Channel2::Id(1));
+  Channel2 ch2(channel_buf, Channel2::Id(0));
+  for (int i = 0; i < 2; i++) {
+    int32_t signal = rand();
+  
+    ch1.Reserve();
+    ch1.SendSignal(Channel2::Id(0), signal);
+    CHECK_EQUAL(signal, ch2.CheckIfNewSignalArrived().Unwrap());
+    ch2.Return(0);
+    ch1.Release();
+  }
+}
+
+TEST(Channel2, GetReturnValue1) {
+  Channel2 ch1(channel_buf, Channel2::Id(1));
+  Channel2 ch2(channel_buf, Channel2::Id(0));
+  int32_t rval = rand();
+
+  ch1.Reserve();
+  ch1.SendSignal(Channel2::Id(0), 1);
+  ch2.CheckIfNewSignalArrived();
+  ch2.Return(rval);
+  CHECK_EQUAL(rval, ch1.CheckIfReturned().Unwrap());
+}
+
+TEST(Channel2, GetReturnValue2) {
+  Channel2 ch1(channel_buf, Channel2::Id(1));
+  Channel2 ch2(channel_buf, Channel2::Id(0));
+  for(int i = 0; i < 2; i++) {
+    int32_t rval = rand();
+
+    ch1.Reserve();
+    ch1.SendSignal(Channel2::Id(0), 1);
+    ch2.CheckIfNewSignalArrived();
+    ch2.Return(rval);
+    CHECK_EQUAL(rval, ch1.CheckIfReturned().Unwrap());
+    ch1.Release();
+  }
+}
+
+TEST(Channel2, UnableToReserveReservedChannel) {
+  Channel2 ch1(channel_buf, Channel2::Id(1));
+  Channel2 ch2(channel_buf, Channel2::Id(0));
+  
+  ch1.Reserve();
+  CHECK(ch2.Reserve().IsError());
+}
+
+TEST(Channel2, MultipleReceiver) {
+  Channel2 ch1(channel_buf, Channel2::Id(0));
+  Channel2 ch2(channel_buf, Channel2::Id(1));
+  Channel2 ch3(channel_buf, Channel2::Id(2));
+
+  ch1.Reserve();
+  ch1.SendSignal(Channel2::Id(2), 1);
+  ch2.CheckIfNewSignalArrived().IsError();
+  CHECK_FALSE(ch3.CheckIfNewSignalArrived().IsError());
+}
+
+TEST(Channel2, SomeoneHasToReceive) {
+  Channel2 ch1(channel_buf, Channel2::Id(0));
+  Channel2 ch2(channel_buf, Channel2::Id(1));
+  ch2.Reserve();
+  ch2.SendSignal(Channel2::Id(2), 1);
+  CHECK(ch1.CheckIfNewSignalArrived().IsError());
+}
+
+TEST(Channel2, ReleaseBeforeReserving) {
+  Channel2 ch1(channel_buf, Channel2::Id(1));
+  Channel2 ch2(channel_buf, Channel2::Id(0));
+  ch1.Reserve();
+  ch1.SendSignal(Channel2::Id(0), 1);
+  ch2.CheckIfNewSignalArrived();
+  ch2.Return(0);
+  ch1.CheckIfReturned();
+
+  CHECK(ch1.Reserve().IsError());
+}
+
+//
+// Do not write following patterns
+//
+TEST(Channel2, ReserveBeforeSendingSignal) {
+  Channel2 ch(channel_buf, Channel2::Id(1));
+  
+  mock().expectOneCall("assert");
+  ch.SendSignal(Channel2::Id(0), 1);
+  mock().checkExpectations();
+}
+
+TEST(Channel2, DoNotReleaseIfNonReserved) {
+  Channel2 ch(channel_buf, Channel2::Id(1));
+  
+  mock().expectOneCall("assert");
+  ch.Release();
+  mock().checkExpectations();
+}
+
+TEST(Channel2, DoNotSendSignal0) {
+  Channel2 ch(channel_buf, Channel2::Id(1));
+  ch.Reserve();
+  
+  mock().expectOneCall("assert");
+  ch.SendSignal(Channel2::Id(0), 0);
+  mock().checkExpectations();
+}
+
