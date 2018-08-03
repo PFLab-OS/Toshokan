62ec54c1a27a7b57083abdc775b15e74881370b1
liva <sap.pcmail@gmail.com>
2018-08-03 23:48:01 +0900

    update questionGimmick

--- ext.js
+++ ext.js
@@ -57,34 +57,33 @@
         return $links.each (function (i, link){
             var $link = $(link);
             var $div = $('<div>');
-            var num = opt.num ? opt.num : 2;
             var $answer = $('<p>').css({
                 display: 'none',
             });
-            for (var i = 0; i < num; i++) {
+            var id = Math.floor(1000000000000*Math.random()).toString(16);
+            for (var i = 0; i < opt.list.length; i++) {
                 var $p = $('<p>');
                 var $input = $('<input>').attr({
                     type: 'radio',
-                    name: 'hyouka',
-                    value: 'hogehoge',
+                    name: id,
                 }).css({
                     "margin-right": "1em",
                 }).on('click',function(my){
-                    if (opt.answer) {
+                    if (opt.answer && opt.answer < opt.list.length) {
                         if (my == opt.answer) {
                             $answer.text("正解！");
                         } else {
                             $answer.text("不正解（正解は" + (opt.answer + 1) + "）");
                         }
                     } else {
-                        $answer.text("エラー：解答が設定されていません。");
+                        $answer.text("エラー：正解が設定されていません");
                     }
                     $answer.css({
                         display: 'block',
                     });
                 }.bind(null, i));
                 $p.append($input);
-                $input.after(i + 1);
+                $input.after((i + 1) + ": " + opt.list[i]);
                 $div.append($p);
             }
             $div.append($answer);
