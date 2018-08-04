6efc9d0e302faacadeaaeecca58ed4158c56d229
liva <sap.pcmail@gmail.com>
2018-08-04 16:46:09 +0900

    add sample/paging/031

---
+++ sample/paging/031/app2.cc
@@ -0,0 +1,20 @@
+#include "app.h"
+
+extern "C" Context entry3(Context c) __attribute__((section(".text.entry")));
+Context entry4(Context c);
+
+Context entry3(Context c) {
+  c.i = 9;
+  c.next = entry4;
+  return c;
+}
+
+Context entry4(Context c) {
+  char *buf = (char *)0xC0001000;
+  for(int i = 0; i < 10; i++) {
+    buf[i] = i;
+  }
+  c.i--;
+  c.next = nullptr;
+  return c;
+}
