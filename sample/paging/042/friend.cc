5ffb4ae8703d419e3ee3bf35f6680b0572061e3f
liva <sap.pcmail@gmail.com>
2018-08-12 19:12:22 +0900

    edit sample/paging/042/friend.cc

--- sample/paging/042/friend.cc
+++ sample/paging/042/friend.cc
@@ -7,28 +7,58 @@ void show_i(F2H &f2h, int i) {
   puts(f2h, mes);
 }
 
+void create_pagetable(uint64_t *page_structures, uint64_t *pd_for_baremetal, uint64_t page_for_app) {
+  // how to use 16KB
+  // 0-4KB: PML4T
+  // 4KB-8KB: PDPT
+  // 8KB-12KB: PD
+  // 12KB-16KB: PT
+  uint64_t *pml4t = page_structures;
+  uint64_t *pdpt = (uint64_t *)((uint64_t)page_structures + 0x1000);
+  uint64_t *pd = (uint64_t *)((uint64_t)page_structures + 0x2000);
+  uint64_t *pt = (uint64_t *)((uint64_t)page_structures + 0x3000);
+
+  pml4t[0] = ((uint64_t)pdpt + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
+  pdpt[0] = ((uint64_t)pd_for_baremetal + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
+  pdpt[3] = ((uint64_t)pd + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
+  pd[0] = ((uint64_t)pt + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
+  pt[0] = page_for_app | (1 << 0) | (1 << 1) | (1 << 2);
+}
+
+void map_page(uint64_t *page_structures) {
+  uint64_t *pt = (uint64_t *)((uint64_t)page_structures + 0x3000);
+  pt[1] = 0x80700000UL | (1 << 0) | (1 << 1) | (1 << 2);
+  asm volatile("invlpg (%0)" ::"b"(0xC0000000UL) : "memory");
+}
+
+void unmap_page(uint64_t *page_structures) {
+  uint64_t *pt = (uint64_t *)((uint64_t)page_structures + 0x3000);
+  pt[1] = 0;
+  asm volatile("invlpg (%0)" ::"b"(0xC0000000UL) : "memory");
+}
+
 int main() {
   F2H f2h;
 
   puts(f2h, "hello!");
 
-  memset((void *)0x310000UL, 0, 0x5000);
+  memset((void *)0x310000UL, 0, 0x9000);
 
-  uint64_t *pml4t = (uint64_t *)0x310000UL;
-  uint64_t *pdpt = (uint64_t *)0x311000UL;
-  uint64_t *pd1 = (uint64_t *)0x312000UL;
-  uint64_t *pd2 = (uint64_t *)0x313000UL;
-  uint64_t *pt1 = (uint64_t *)0x314000UL;
+  uint64_t *page_structures_for_app1 = (uint64_t *)0x310000UL; // 16KB (for PML4T, PDPT, PD, PT)
+  uint64_t *page_structures_for_app2 = (uint64_t *)0x314000UL; // 16KB (for PML4T, PDPT, PD, PT)
+  uint64_t *pd_for_baremetal = (uint64_t *)0x318000UL;
 
-  pml4t[0] = ((uint64_t)pdpt + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
-  pdpt[0] = ((uint64_t)pd1 + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
-  pdpt[3] = ((uint64_t)pd2 + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
-  pd2[0] = ((uint64_t)pt1 + 0x80000000UL) | (1 << 0) | (1 << 1) | (1 << 2);
+  // page directory of virtual address 0 - 1GB (for friend.cc)
+  // shared by app1 and app2
   for (int i = 0; i < 512; i++) {
-    pd1[i] = (0x80000000UL + 0x200000UL * i) | (1 << 0) | (1 << 1) | (1 << 2) |
-             (1 << 7);
+    pd_for_baremetal[i] = (0x80000000UL + 0x200000UL * i) | (1 << 0) | (1 << 1) | (1 << 2) |
+      (1 << 7) | (1 << 8);
   }
-  asm volatile("movq %0, %%cr3" ::"r"((uint64_t)pml4t + 0x80000000UL));
+  
+  create_pagetable(page_structures_for_app1, pd_for_baremetal, 0x80500000UL);
+  create_pagetable(page_structures_for_app2, pd_for_baremetal, 0x80600000UL);
+  uint64_t *pml4t_for_app1 = page_structures_for_app1;
+  uint64_t *pml4t_for_app2 = page_structures_for_app2;
 
   extern uint8_t _binary_app1_start[];
   extern uint8_t _binary_app1_size[];
@@ -43,11 +73,10 @@ int main() {
   c1.next = (Context(*)(Context))0xC0000000UL;  // entry1
   c1.i = 0;
 
-  pt1[0] = 0x80500000UL | (1 << 0) | (1 << 1) | (1 << 2);
-  asm volatile("invlpg (%0)" ::"b"(0xC0000000UL) : "memory");
-  pt1[1] = 0x80700000UL | (1 << 0) | (1 << 1) | (1 << 2);
-  asm volatile("invlpg (%0)" ::"b"(0xC0001000UL) : "memory");
+  asm volatile("movq %0, %%cr3" ::"r"((uint64_t)pml4t_for_app1 + 0x80000000UL));
+  map_page(page_structures_for_app1);
   c1 = c1.next(c1);  // call entry1
+  unmap_page(page_structures_for_app1);
 
   show_i(f2h, c1.i);
 
@@ -60,10 +89,7 @@ int main() {
   c2.next = (Context(*)(Context))0xC0000000UL;  // entry3
   c2.i = 0;
 
-  pt1[0] = 0x80600000UL | (1 << 0) | (1 << 1) | (1 << 2);
-  asm volatile("invlpg (%0)" ::"b"(0xC0000000UL) : "memory");
-  pt1[1] = 0;
-  asm volatile("invlpg (%0)" ::"b"(0xC0001000UL) : "memory");
+  asm volatile("movq %0, %%cr3" ::"r"((uint64_t)pml4t_for_app2 + 0x80000000UL));
   c2 = c2.next(c2);  // call entry3
 
   show_i(f2h, c2.i);
@@ -73,10 +99,7 @@ int main() {
     return 1;
   }
 
-  pt1[0] = 0x80500000UL | (1 << 0) | (1 << 1) | (1 << 2);
-  asm volatile("invlpg (%0)" ::"b"(0xC0000000UL) : "memory");
-  pt1[1] = 0;
-  asm volatile("invlpg (%0)" ::"b"(0xC0001000UL) : "memory");
+  asm volatile("movq %0, %%cr3" ::"r"((uint64_t)pml4t_for_app1 + 0x80000000UL));
   c1 = c1.next(c1);  // call entry2
 
   show_i(f2h, c1.i);
@@ -86,11 +109,10 @@ int main() {
     return 1;
   }
 
-  pt1[0] = 0x80600000UL | (1 << 0) | (1 << 1) | (1 << 2);
-  asm volatile("invlpg (%0)" ::"b"(0xC0000000UL) : "memory");
-  pt1[1] = 0x80700000UL | (1 << 0) | (1 << 1) | (1 << 2);
-  asm volatile("invlpg (%0)" ::"b"(0xC0001000UL) : "memory");
+  asm volatile("movq %0, %%cr3" ::"r"((uint64_t)pml4t_for_app2 + 0x80000000UL));
+  map_page(page_structures_for_app2);
   c2 = c2.next(c2);  // call entry4
+  unmap_page(page_structures_for_app2);
 
   show_i(f2h, c2.i);
 
--- sample/paging/043/app.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-struct Context {
-  Context (*next)(Context c);
-  int i;
-};
