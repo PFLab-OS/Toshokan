d6069b5f75a8c977dc0f9a129d73c47a6572afc8
liva <sap.pcmail@gmail.com>
2018-08-12 19:42:15 +0900

    add comment

--- sample/paging/042/app1.cc
+++ sample/paging/042/app1.cc
@@ -4,6 +4,7 @@ extern "C" Context entry1(Context c) __attribute__((section(".text.entry")));
 Context entry2(Context c);
 
 Context entry1(Context c) {
+  // invokes the system call which maps physical memory
   char *buf = (char *)0xC0001000;
   for (int i = 0; i < 10; i++) {
     buf[i] = i;
--- sample/paging/042/app2.cc
+++ sample/paging/042/app2.cc
@@ -10,6 +10,7 @@ Context entry3(Context c) {
 }
 
 Context entry4(Context c) {
+  // invokes the system call which maps physical memory
   char *buf = (char *)0xC0001000;
   for (int i = 0; i < 10; i++) {
     buf[i] = i;
