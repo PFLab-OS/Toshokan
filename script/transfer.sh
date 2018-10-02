acf8ef10cbd444973511851e287f69ef9c2570aa
Shinichi Awamoto <sap.pcmail@gmail.com>
2018-10-02 22:49:24 +0900

    enable transfer.sh to get file lists as arguments

--- script/transfer.sh
+++ script/transfer.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -x
 
-TAG=$1
+SFTP_COMMAND=$1
 shift
 echo $@
 COMMAND=`for f in $@; do echo "put $f share"; done`
-docker run --rm -v $PWD:/workdir -w /workdir -i --network toshokan_net livadk/toshokan_ssh:$TAG sftp -o ConnectTimeout=3 -o LogLevel=quiet -o StrictHostKeyChecking=no -o GlobalKnownHostsFile=/dev/null -o UserKnownHostsFile=/dev/null -i /id_rsa -P 2222 hakase@toshokan_qemu <<EOF
+$SFTP_COMMAND <<EOF
 mkdir share
 put hakase/tests/test_hakase.sh share
 put hakase/tests/test_library.sh share
