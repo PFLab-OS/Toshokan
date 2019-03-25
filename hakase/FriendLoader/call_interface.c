f340d7d1fd19802be8fae7f225c7b41852217c34
Shinichi Awamoto <sap.pcmail@gmail.com>
2019-03-26 01:00:09 +0900

    add straight mapped memory interface

--- hakase/FriendLoader/call_interface.c
+++ hakase/FriendLoader/call_interface.c
@@ -19,6 +19,10 @@ static int call_i2h_mmap(struct file *filep, struct kobject *kobj,
                          struct bin_attribute *attr,
                          struct vm_area_struct *vma);
 
+static int call_mem_mmap(struct file *filep, struct kobject *kobj,
+                         struct bin_attribute *attr,
+                         struct vm_area_struct *vma);
+
 static struct bin_attribute call_h2f_attr = {
     .attr =
         {
@@ -46,8 +50,17 @@ static struct bin_attribute call_i2h_attr = {
     .mmap = call_i2h_mmap,
 };
 
+static struct bin_attribute call_mem_attr = {
+    .attr =
+        {
+            .name = "mem", .mode = S_IWUSR | S_IRUGO,
+        },
+    .size = DEPLOY_PHYS_MEM_SIZE,
+    .mmap = call_mem_mmap,
+};
+
 static struct bin_attribute *call_sysfs_attrs[] = {
-    &call_h2f_attr, &call_f2h_attr, &call_i2h_attr, NULL,
+  &call_h2f_attr, &call_f2h_attr, &call_i2h_attr, &call_mem_attr, NULL,
 };
 static struct attribute_group call_sysfs_attr_group = {
     .bin_attrs = call_sysfs_attrs,
@@ -139,3 +152,40 @@ static int call_i2h_mmap(struct file *filep, struct kobject *kobj,
 
   return 0;
 }
+
+// from drivers/char/mem.c
+static int call_mem_mmap(struct file *filep, struct kobject *kobj,
+                         struct bin_attribute *attr,
+                         struct vm_area_struct *vma) {
+  size_t size = vma->vm_end - vma->vm_start;
+  phys_addr_t phys_start = (phys_addr_t)vma->vm_pgoff << PAGE_SHIFT;
+  phys_addr_t phys_end = phys_start + (phys_addr_t)size;
+  
+  /* It's illegal to wrap around the end of the physical address space. */
+  if (phys_end - 1 < phys_start) {
+    return -EINVAL;
+  }
+
+  if (phys_start > DEPLOY_PHYS_MEM_SIZE || phys_end > DEPLOY_PHYS_MEM_SIZE) {
+    return -EINVAL;
+  }
+
+#ifdef ARCH_HAS_VALID_PHYS_ADDR_RANGE
+  if (!valid_mmap_phys_addr_range(vma->vm_pgoff, size))
+    return -EINVAL;
+#endif
+  
+  vma->vm_ops = &mmap_vm_ops;
+
+  /* Remap-pfn-range will mark the range VM_IO */
+  if (remap_pfn_range(vma,
+		      vma->vm_start,
+		      vma->vm_pgoff,
+		      size,
+		      vma->vm_page_prot)) {
+    pr_info("friend loader: mmap %d\n", __LINE__);
+    return -EAGAIN;
+  }
+  return 0;
+}
+
