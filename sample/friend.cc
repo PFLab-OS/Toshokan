69ce7105432156bfb7d8f665c7c3f2d24d7d5e04
Shinichi Awamoto <sap.pcmail@gmail.com>
2019-06-11 02:12:50 +0200

    update sample

--- sample/friend.cc
+++ sample/friend.cc
@@ -1,6 +1,10 @@
 #include "shared.h"
 
-int64_t sync_flag;
-Page pml4t;
-Page pdpt;
-Page pd;
+int64_t SHARED_SYMBOL(sync_flag);
+Page SHARED_SYMBOL(pml4t);
+Page SHARED_SYMBOL(pdpt);
+Page SHARED_SYMBOL(pd);
+
+extern "C" void friend_main() {
+  __sync_fetch_and_add(&SHARED_SYMBOL(sync_flag), 1);
+}
