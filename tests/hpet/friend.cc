a5094ec8e47a23e21007458d195a95f8b1409bda
Shinichi Awamoto <sap.pcmail@gmail.com>
2019-06-26 18:02:24 +0200

    FSB test

---
+++ tests/hpet/friend.cc
@@ -0,0 +1,17 @@
+#include <toshokan/friend/export.h>
+#include <toshokan/friend/offload.h>
+#include "shared.h"
+
+int64_t SHARED_SYMBOL(sync_flag);
+void (*EXPORTED_SYMBOL(func))(int i, int j);
+int (*EXPORTED_SYMBOL(printf))(const char *format, ...);
+
+void friend_main()
+{
+  OFFLOAD_FUNC(func, 11, 10);
+  uint64_t xxx = *((uint64_t *)0xfed00000);
+  OFFLOAD_FUNC(printf, "<%lx>\n", xxx);
+  xxx = *((uint64_t *)0xfed00140);
+  OFFLOAD_FUNC(printf, "<%lx>\n", xxx);
+  SHARED_SYMBOL(sync_flag) = 1;
+}
