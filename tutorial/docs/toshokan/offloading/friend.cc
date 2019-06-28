aecd8b7bfad1ce4ee348bbe96f9da3f61f58b4b5
Shinichi Awamoto <sap.pcmail@gmail.com>
2019-06-28 17:13:22 +0200

    format

--- tutorial/docs/toshokan/offloading/friend.cc
+++ tutorial/docs/toshokan/offloading/friend.cc
@@ -4,9 +4,10 @@
 int (*EXPORTED_SYMBOL(printf))(const char *format, ...);
 int SHARED_SYMBOL(sync_flag) = 0;
 
-void friend_main()
-{
-  OFFLOAD({ EXPORTED_SYMBOL(printf)
-            ("%s\n", "Hello World!"); });
+void friend_main() {
+  OFFLOAD({
+    EXPORTED_SYMBOL(printf)
+    ("%s\n", "Hello World!");
+  });
   SHARED_SYMBOL(sync_flag) = 1;
 }
--- tutorial/toshokan/offloading/shared.h
+++ tutorial/toshokan/offloading/shared.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <toshokan/symbol.h>
 #include <toshokan/export.h>
+#include <toshokan/symbol.h>
 
 extern int SHARED_SYMBOL(sync_flag);
 extern int (*EXPORTED_SYMBOL(printf))(const char *format, ...);
