846b9c45ec9eeb57214773a98b90d71d97785a1d
Shinichi Awamoto <sap.pcmail@gmail.com>
2019-07-04 18:04:14 +0200

    add files

---
+++ tutorial/docs/paging/address_space/friend.cc
@@ -0,0 +1,43 @@
+#include <toshokan/friend/offload.h>
+#include "shared.h"
+
+int (*EXPORTED_SYMBOL(printf))(const char *format, ...);
+
+struct Page
+{
+  uint64_t entry[512];
+} __attribute__((aligned(4096)));
+
+Page SHARED_SYMBOL(__toshokan_pdpt);
+Page pd;
+using virt_addr_t = uint64_t;
+using phys_addr_t = uint64_t;
+
+static inline phys_addr_t v2p(virt_addr_t vaddr)
+{
+  // assume physical memory is mapped straight to virtual memory
+  return vaddr;
+}
+
+void friend_main()
+{
+  static const size_t k512GB = 512UL * 1024 * 1024 * 1024;
+  static const size_t k1GB = 1024UL * 1024 * 1024;
+  static const size_t k2MB = 2UL * 1024 * 1024;
+
+  static const virt_addr_t vaddr = 0x80000000;
+  SHARED_SYMBOL(__toshokan_pdpt)
+      .entry[(vaddr % k512GB) / k1GB] =
+      v2p(reinterpret_cast<virt_addr_t>(&pd)) | (1 << 0) |
+      (1 << 1) | (1 << 2);
+  pd.entry[(vaddr % k1GB) / k2MB] = v2p(vaddr) | (1 << 0) | (1 << 1) | (1 << 2) | (1 << 7);
+
+  OFFLOAD({
+    EXPORTED_SYMBOL(printf)
+    ("ready\n");
+  });
+  while (1)
+  {
+    asm volatile("hlt;");
+  }
+}
---
+++ tutorial/paging/address_space/README.md.tpl
@@ -0,0 +1,11 @@
+{% import 'macro.tpl' as helper %}
+# 仮想メモリと物理メモリ
+
+{{ helper.sample_info() }}
+
+## QEMUモニタ上での物理メモリの確認
+
+
+## QEMUモニタ上での仮想メモリの確認
+
+
