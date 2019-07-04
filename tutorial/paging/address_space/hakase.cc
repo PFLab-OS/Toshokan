99b347175b746a123bfacf29276207588cefe658
Shinichi Awamoto <sap.pcmail@gmail.com>
2019-07-04 18:04:38 +0200

    format

--- tutorial/paging/address_space/hakase.cc
+++ tutorial/paging/address_space/hakase.cc
@@ -6,34 +6,27 @@
 
 EXPORT_SYMBOL(printf);
 
-int test_main()
-{
+int test_main() {
   int r;
   r = setup();
-  if (r != 0)
-  {
+  if (r != 0) {
     return r;
   }
 
   boot(1);
 
-  while (!is_friend_stopped())
-  {
+  while (!is_friend_stopped()) {
     offloader_tryreceive();
     usleep(1000);
   }
   return 1;
 }
 
-int main(int argc, const char **argv)
-{
-  if (test_main() > 0)
-  {
+int main(int argc, const char **argv) {
+  if (test_main() > 0) {
     printf("\e[32m%s: PASSED\e[m\n", argv[0]);
     return 0;
-  }
-  else
-  {
+  } else {
     printf("\e[31m%s: FAILED\e[m\n", argv[0]);
     return 255;
   }
