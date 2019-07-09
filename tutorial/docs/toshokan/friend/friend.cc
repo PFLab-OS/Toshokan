903c7960bff281ce64acf733961971c16efb048a
Shinichi Awamoto <sap.pcmail@gmail.com>
2019-07-09 08:36:01 +0200

    add another tutorial page

---
+++ tutorial/docs/toshokan/friend/friend.cc
@@ -0,0 +1,27 @@
+#include <toshokan/friend/offload.h>
+#include "shared.h"
+
+int (*EXPORTED_SYMBOL(printf))(const char *format, ...);
+int SHARED_SYMBOL(variable) = 0;
+
+void friend_main() {
+  /*
+   * do not run these code on friend
+   *
+   
+   printf("hello world\n");
+   
+   */
+  asm volatile("cli;");
+  OFFLOAD({
+      /*
+       * do not run these code in offloading section
+       *
+   
+       asm volatile("cli;");
+   
+      */
+      EXPORTED_SYMBOL(printf)("Hello World!\n");
+  });
+  SHARED_SYMBOL(variable) = 1;
+}
---
+++ tutorial/docs/toshokan/friend/shared.h
@@ -0,0 +1,4 @@
+#include <toshokan/export.h>
+
+extern int (*EXPORTED_SYMBOL(printf))(const char *format, ...);
+extern int SHARED_SYMBOL(variable);
