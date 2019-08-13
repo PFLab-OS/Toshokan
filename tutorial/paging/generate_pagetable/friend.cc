c89097ff26a56d631622cbac0de76eb04e3fa3b9
Shinichi Awamoto <sap.pcmail@gmail.com>
2019-08-13 09:52:26 +0200

    format & doc fix

--- tutorial/paging/generate_pagetable/friend.cc
+++ tutorial/paging/generate_pagetable/friend.cc
@@ -24,23 +24,13 @@ void friend_main() {
   static const size_t k2MB = 2UL * 1024 * 1024;
   static const size_t k4KB = 4UL * 1024;
 
-  pml4t.entry[0] = v2p((virt_addr_t)&pdpt) | (1 << 0) | (1 << 1) | (1 << 2);
-  pdpt.entry[1] = v2p((virt_addr_t)&pd) | (1 << 0) | (1 << 1) | (1 << 2);
+  // write your code here
 
-  for (int i = 0; i < 512; i++) {
-    pd.entry[i] = v2p((virt_addr_t)&pt[i]) | (1 << 0) | (1 << 1) | (1 << 2);
-    for (int j = 0; j < 512; j++) {
-      pt[i].entry[j] = (0x40000000 + i * 0x200000 + j * 0x1000) | (1 << 0) |
-                       (1 << 1) | (1 << 2);
-    }
-  }
   asm volatile("movq %0, %%cr3" ::"r"(v2p((virt_addr_t)&pml4t)));
   uint64_t x = *((uint64_t *)0x40000000);
 
   OFFLOAD({
     EXPORTED_SYMBOL(printf)
     ("0x%lx: %lx\n", 0x40000000L, x);
-    EXPORTED_SYMBOL(printf)
-    ("ready\n");
   });
 }
