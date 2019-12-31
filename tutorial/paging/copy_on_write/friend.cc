813e4cff325e684e9b369cd2cf33ace8d6df6913
Shinichi Awamoto <sap.pcmail@gmail.com>
2019-12-31 18:57:54 +0900

    format

--- tutorial/paging/copy_on_write/friend.cc
+++ tutorial/paging/copy_on_write/friend.cc
@@ -57,21 +57,23 @@ static const size_t k4KB = 4UL * 1024;
 static inline void my_memcpy(void *dst, void *src, size_t n) {
   uint64_t *dst_ = (uint64_t *)dst;
   uint64_t *src_ = (uint64_t *)src;
-  for(size_t i = 0; i < n / sizeof(uint64_t); i += sizeof(uint64_t)) {
+  for (size_t i = 0; i < n / sizeof(uint64_t); i += sizeof(uint64_t)) {
     dst_[i] = src_[i];
   }
 }
 
 extern "C" void fault_handler() {
   uint64_t fault_address;
-  asm volatile("movq %%cr2, %0;":"=r"(fault_address));
+  asm volatile("movq %%cr2, %0;" : "=r"(fault_address));
   // we assume that fault address is managed by 'pt'.
   // and the entry is higher than 256.
   int fault_entry = (fault_address % k2MB) / k4KB;
-  pt.entry[fault_entry] = (0x50000000 + fault_entry * 0x1000) | (1 << 0) | (1 << 1) | (1 << 2);
+  pt.entry[fault_entry] =
+      (0x50000000 + fault_entry * 0x1000) | (1 << 0) | (1 << 1) | (1 << 2);
   int src_entry = fault_entry - 256;
   pt.entry[src_entry] |= (1 << 1);
-  my_memcpy((void *)(0x50000000 + fault_entry * 0x1000), (void *)(0x50000000 + src_entry * 0x1000), 0x1000);
+  my_memcpy((void *)(0x50000000 + fault_entry * 0x1000),
+            (void *)(0x50000000 + src_entry * 0x1000), 0x1000);
   asm volatile("invlpg (%0)" ::"r"(fault_address) : "memory");
 }
 
@@ -86,40 +88,34 @@ void friend_main() {
   pd.entry[(vaddr1 % k1GB) / k2MB] =
       v2p((virt_addr_t)(&pt)) | (1 << 0) | (1 << 1) | (1 << 2);
   for (int i = 0; i < 256; i++) {
-    pt.entry[i] =
-        (0x50000000 + i * 0x1000) | (1 << 0) | (1 << 1) | (1 << 2);
+    pt.entry[i] = (0x50000000 + i * 0x1000) | (1 << 0) | (1 << 1) | (1 << 2);
   }
 
   {
-  uint64_t t1 = rdtscp();
-  for (int i = 256; i < 512; i++) {
-    pt.entry[i] =
-        (0x50000000 + i * 0x1000) | (1 << 0) | (1 << 1) | (1 << 2);
-  }
-  my_memcpy((void *)vaddr2, (void *)vaddr1, 0x100000);
-  for(int i = 0; i < 256; i++) {
-    *((uint64_t *)(0x80100000 + i * 0x1000)) = 0;
-  }
-  uint64_t t2 = rdtscp();
-  OFFLOAD({
-      EXPORTED_SYMBOL(printf)("%d\n", t2 - t1);
-    });
+    uint64_t t1 = rdtscp();
+    for (int i = 256; i < 512; i++) {
+      pt.entry[i] = (0x50000000 + i * 0x1000) | (1 << 0) | (1 << 1) | (1 << 2);
+    }
+    my_memcpy((void *)vaddr2, (void *)vaddr1, 0x100000);
+    for (int i = 0; i < 256; i++) {
+      *((uint64_t *)(0x80100000 + i * 0x1000)) = 0;
+    }
+    uint64_t t2 = rdtscp();
+    OFFLOAD({ EXPORTED_SYMBOL(printf)("%d\n", t2 - t1); });
   }
 
   {
-  uint64_t t1 = rdtscp();
-  for (int i = 0; i < 256; i++) {
-    pt.entry[i] = (0x50000000 + i * 0x1000) | (1 << 0) | (1 << 2);
-  }
-  for (int i = 256; i < 512; i++) {
-    pt.entry[i] = pt.entry[i - 256];
-  }
-  for(int i = 0; i < 256; i++) {
-    *((uint64_t *)(0x80100000 + i * 0x1000)) = 0;
-  }
-  uint64_t t2 = rdtscp();
-  OFFLOAD({
-      EXPORTED_SYMBOL(printf)("%d\n", t2 - t1);
-    });
+    uint64_t t1 = rdtscp();
+    for (int i = 0; i < 256; i++) {
+      pt.entry[i] = (0x50000000 + i * 0x1000) | (1 << 0) | (1 << 2);
+    }
+    for (int i = 256; i < 512; i++) {
+      pt.entry[i] = pt.entry[i - 256];
+    }
+    for (int i = 0; i < 256; i++) {
+      *((uint64_t *)(0x80100000 + i * 0x1000)) = 0;
+    }
+    uint64_t t2 = rdtscp();
+    OFFLOAD({ EXPORTED_SYMBOL(printf)("%d\n", t2 - t1); });
   }
 }
