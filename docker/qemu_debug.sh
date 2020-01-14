76ed6d0cc301c52c228d96c6a4f11c513c5c8072
Shinichi Awamoto <sap.pcmail@gmail.com>
2020-01-14 19:13:46 +0900

    bug fix

--- docker/qemu_debug.sh
+++ docker/qemu_debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 apt update -y
-apt install -y openssh-client rsync busybox-static
+apt install -y openssh-client rsync busybox-static gdb
 apt install -y build-essential zlib1g-dev pkg-config libglib2.0-dev binutils-dev libboost-all-dev autoconf libtool libssl-dev libpixman-1-dev libpython-dev python-pip python-capstone virtualenv git flex bison
 mkdir /root/.ssh
 cp /mnt/id_rsa /root/.ssh/id_rsa
@@ -11,7 +11,7 @@ chmod 600 /root/.ssh/id_rsa
 
 mkdir -p /share/.qemu-build
 pushd /share/.qemu-build
-/share/qemu/configure --target-list=x86_64-softmmu --enable-debug
+/share/qemu/configure --target-list=x86_64-softmmu --enable-debug --disable-pie
 make -j8
 make install
 popd
