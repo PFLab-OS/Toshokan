e3b442ce968113dfc106f687c03c55dfd0495f1b
Shinichi Awamoto <sap.pcmail@gmail.com>
2020-02-16 18:08:45 +0900

    bug fix

--- docker/qemu_debug.sh
+++ docker/qemu_debug.sh
@@ -24,7 +24,7 @@ cp -r /share/rootfs /
 mkdir build
 cp /share/friend_loader.ko build
 qemu-img create -f qcow2 /backing.qcow2 5G
-qemu-system-x86_64 -cpu Haswell -d cpu_reset -no-reboot -smp 5 -m 4G -D /tmp/qemu.log -hda /backing.qcow2 -kernel /vmlinuz-4.13.0-45-generic -initrd /rootfs -append 'root=/dev/ram rdinit=/sbin/init memmap=0x70000$8K memmap=0x100000000$0x40000000 console=ttyS0,115200' -net nic -net user,hostfwd=tcp::2222-:22 telnet::4445,server,nowait -nographic -global hpet.msi=true -serial telnet::4444,server,nowait -monitor > /dev/null 2>&1 &
+qemu-system-x86_64 -cpu Haswell -d cpu_reset -no-reboot -smp 5 -m 4G -D /tmp/qemu.log -hda /backing.qcow2 -kernel /vmlinuz-4.13.0-45-generic -initrd /rootfs -append 'root=/dev/ram rdinit=/sbin/init memmap=0x70000$8K memmap=0x100000000$0x40000000 console=ttyS0,115200' -net nic -net user,hostfwd=tcp::2222-:22 -monitor telnet::4445,server,nowait -nographic -global hpet.msi=true -serial telnet::4444,server,nowait > /dev/null 2>&1 &
 while ! sh -c "rsync --list-only toshokan_qemu: > /dev/null 2>&1" ; do
     sleep 1;
 done
